15ce0b1e15c4cf31beaacadb0d289a41288257b0
sawasawa0 <sawasawaworks@gmail.com>
2017-07-20 11:31:36 +0000

    rooter difined

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MessagesContoroller@index');
+Route::resource('messages' , 'MessagesContoroller');
+
+
+/*Root::resourse()でルーティングされるもの
+
+// CRUD
+Route::get('messages/{id}', 'MessagesController@show');
+Route::post('messages', 'MessagesController@store');
+Route::put('messages/{id}', 'MessagesController@update');
+Route::delete('messages/{id}', 'MessagesController@destroy');
+
+// index: showの補助ページ
+Route::get('messages', 'MessagesController@index');
+
+// create: 新規作成用のフォームページ
+Route::get('messages/create', 'MessagesController@create');
+
+// edit: 更新用のフォームページ
+Route::get('messages/{id}/edit', 'MessagesController@edit');
+
+*/
