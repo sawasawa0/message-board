f998d4c1d2f8f4a6809d574493ca34261506ad9e
sawasawa0 <sawasawaworks@gmail.com>
2017-07-23 07:02:45 +0000

    bootstrap design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,13 +3,25 @@
     <head>
         <meta charset="utf-8">
         <title>MessageBoard</title>
+        
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
     </head>
 
 
     <body>
+        @include('commons.navbar')
         
-        @include('commons.error_messages')
-        
-        @yield('content')
+        <div class="container">
+            @include('commons.error_messages')
+            
+            @yield('content')
+        </div>
     </body>
 </html>
